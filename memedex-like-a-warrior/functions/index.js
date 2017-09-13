1d0dd3d4a357c0674e26ec986bc5719f0e863d7b
DezkaReid <elmaildeldezkareid@gmail.com>
2017-09-13 13:39:01 -0500

    Added cors support

--- memedex-like-a-warrior/functions/index.js
+++ memedex-like-a-warrior/functions/index.js
@@ -1,28 +1,32 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
+
 admin.initializeApp(functions.config().firebase);
 
 exports.suscribe = functions.https.onRequest((request, response) => {
-	let token = request.body && request.body.token;
-	if (token) {
-		admin.messaging().subscribeToTopic([token], 'general')
-			.then(function () {
-				let notification = {
-					"data": {
-						"title" : "Bienvenido",
-						"body" : "Centraal Academy te recibe"
-					}
-				};
-				return admin.messaging().sendToDevice([token], notification)
-			})
-			.then(function () {
-				response.status(200).end();
-			})
-			.catch(function (error) {
-				response.status(500).end();
-				console.error("Hubo un error", error);
-			});
-	} else {
-		response.status(403).send('Bad Request');;
-	}
+	cors(request, response, function () {
+		let token = request.body && request.body.token;
+		if (token) {
+			admin.messaging().subscribeToTopic([token], 'general')
+				.then(function () {
+					let notification = {
+						"data": {
+							"title" : "Bienvenido",
+							"body" : "Centraal Academy te recibe"
+						}
+					};
+					return admin.messaging().sendToDevice([token], notification)
+				})
+				.then(function () {
+					response.status(200).end();
+				})
+				.catch(function (error) {
+					response.status(500).end();
+					console.error("Hubo un error", error);
+				});
+		} else {
+			response.status(403).send('Bad Request');
+		}
+	})
 });
